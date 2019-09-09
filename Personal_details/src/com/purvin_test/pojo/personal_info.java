92c93c473ec04f13ae0ccb193fd36be517ae8d59
johnden-faltu <55064346+johnden-faltu@users.noreply.github.com>
2019-09-08 21:07:51 -0400

    Initial commit of project

---
+++ Personal_details/src/com/purvin_test/pojo/personal_info.java
@@ -0,0 +1,16 @@
+package com.purvin_test.pojo;
+
+public class personal_info {
+
+	String f_Name;
+	String l_Name;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		personal_info pi = new personal_info();
+		String h1 = pi.f_Name="Pur";
+		String h2 = pi.l_Name="Pat";
+		System.out.println(h1 + h2);
+	}
+
+}
